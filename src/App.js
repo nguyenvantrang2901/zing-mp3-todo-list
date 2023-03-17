49f7eedc56132c538664733f5b1cb6ed9f906cde
TrangNguyenVan <nguyenvantrang2901@gmail.com>
2023-03-17 17:11:24 +0700

    update video 2 - Todo list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [work, setWork] = useState('')
   return (
-    <div className="flex items-center justify-center border border-red-500 h-screen">
-      Zing Mp3
+    <div className="flex gap-8 items-center justify-center border border-red-500 h-screen">
+      <input 
+        type="text" 
+        className='outline-none border border-blue-500 rounded-md px-4 py-2 w-[400px]'
+        placeholder='Add todo...' 
+        value={work} 
+        onChange={e=>setWork(e.target.value)}
+        />
+      <button 
+        type='button' 
+        className='outline-none bg-blue-500 rounded-md text-white py-2'
+      >
+        Add new
+      </button>
     </div>
   );
 }
