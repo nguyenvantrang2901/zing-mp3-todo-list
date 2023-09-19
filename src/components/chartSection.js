abcdeb357a785dd5433d5bc1440529f3183b224e
TrangNguyenVan <nguyenvantrang2901@gmail.com>
2023-09-19 15:18:21 +0700

    code zingchart part 2

--- src/components/chartSection.js
+++ src/components/chartSection.js
@@ -13,12 +13,17 @@ const ChartSection = () => {
     console.log("rank" ,{ rank})
     const options = {
         responsive : true,
-        pointRadius : 6,
-        aspectRatio : 4, //độ dài x=4y
+        pointRadius : 2,
+        maintainAspectRatio: false,
         scales:{
             y:{
                 ticks : { display: false},
-                grid : { borderDash: [4,4], color : "rgba(255,255, 255,0.5)"}
+                grid : { color : "rgba(255,255, 255,0.5)", drawTicks: false}, //drawTicks:là bỏ đi phần viền ở đầu mỗi hàng
+                min: chart?.minScore,
+                max : chart?.maxScore,
+
+                //viền nét đứt 3 là chiều dài của nét đứt. 4 là khoảng cách giữa 2 nét đứt
+                border: {dash:[3,4]} 
             },
             x : {
                 ticks : { color : "white"},
@@ -30,11 +35,15 @@ const ChartSection = () => {
         },
         plugins : {
             legend : false
+        },
+        hover: {
+            mode: "dataset",
+            intersect: false
         }
     }
     
     useEffect(()=>{
-        const labels = chart?.times?.filter(item=> +item.hour %2 ===0)?.map(item => item.hour)
+        const labels = chart?.times?.filter(item=> +item.hour %2 ===0)?.map(item => `${item.hour}:00`)
         const datasets = []
         if(chart?.items) {
             for (let i=0; i<3; i++){
@@ -42,7 +51,11 @@ const ChartSection = () => {
                     data : chart?.items[Object.keys(chart?.items)[i]]?.filter(item=> +item.hour % 2 ===0)?.map(item => item.counter),
                     borderColor : i === 0 ? "#4A90E2" : i === 1 ? "#27BD9C" : "#E35050",
                     tension : 0.2,
-                    borderWidth: 2
+                    borderWidth: 2,
+                    pointBackgroundColor: "white",
+                    pointHoverRadius: 4,
+                    pointBorderColor: i === 0 ? "#4A90E2" : i === 1 ? "#27BD9C" : "#E35050",
+                    pointHoverBorderWidth: 4
                 })
             }
             console.log({labels, datasets})
@@ -50,28 +63,31 @@ const ChartSection = () => {
         }
     },[chart])
   return (
-    <div className='mt-12 px-[59px] relative'>
+    <div className='mt-12 px-[59px] relative max-h-[400px]'>
         <img src={bgChart2} alt="background Chart" className='w-full max-h-[400px]'/>
         <div className='absolute top-0 bottom-0 z-20 right-[59px] left-[59px] bg-[rgba(77,34,104,0.9)]'></div>
-        <div className="absolute top-0 bottom-0 z-20 right-[59px] left-[59px] p-5">
+        <div className="absolute top-0 bottom-0 z-20 right-[59px] left-[59px] p-5 flex flex-col">
             <div className='flex'>
                 <h3 className='text-2xl text-white font-bold'>#zingchart</h3>
                 <h3><BsPlayFill/></h3>
             </div>
             <div className='flex gap-4 h-full p-[10px]'> 
                 <div className='flex-4 h-[400px] py-[10px]'>
-                    {rank?.filter((item, index)=>index<3)?.map(item=>{
+                    {rank?.filter((item, index)=>index<3)?.map((item, index)=>{
                         return (
                             <RateSongItem
                                 key= {item?.encodeId}
                                 title= {item?.title}
                                 artists = {item?.artists}
                                 thumbnail = {item?.thumbnail}
+                                sid = {item?.encodeId}
+                                order = {index+1}
+                                percent = {Math.round(+item?.score*100 / +chart?.totalScore)}
                             />
                         )
                     })}
                 </div>
-                <div className='flex-6 max-h-[400px]'>
+                <div className='flex-6 h-full pb-[40px]'>
                     {data && <Line data={data} options = {options}/>}
                 </div>
             </div>
--- src/components/rateSongItem.js
+++ src/components/rateSongItem.js
@@ -6,29 +6,32 @@ import { useDispatch } from 'react-redux'
 
 //Truyển props để lấy dữ liệu từ file newRelase xuống.
 //tạo componets SongItem để sử dụng đc nhiều lần
-const RateSongItem = ({thumbnail, artists, title, releaseDate, songData}) => {
+const RateSongItem = ({thumbnail, artists, title, releaseDate, songData, order, percent}) => {
   const dispatch = useDispatch()
   return (
     <div 
-      className='bg-purple-500 flex flex-4 w-[100%] min-[1024px] p-[10px] gap-2 hover:bg-purple-400 rounded-md mb-[10px]'
+      className='justify-between items-center bg-purple-500 flex flex-4 w-[100%] min-[1024px] p-[10px] gap-2 hover:bg-purple-400 rounded-md mb-[10px]'
       onClick={()=>{
             dispatch(actions.setCurSongId(songData?.encodeId))
             dispatch(actions.play(true))
             dispatch(actions.playAlbum(true))
         }} 
       >
-      <h1>#</h1>
-      <img src={thumbnail} alt="Thumbnail" className='w-[60px] h-[60px] object-cover rounded-md' />
-      <div className='flex flex-col'> 
-        <span className='font-semibold text-white text-sm'>{title?.length > 20 ? `${title.slice(0,20)}...` : title}</span>
-        {artists?.map(item=>{
-            return (
-                <>
-                    <span className='text-xs text-gray-300 cursor-pointer'>{item?.name}</span>
-                </>
-            )
-        })}
+      <div className="flex gap-4">
+        <span className="text-bold text-white text-[32px]">{order}</span>
+        <img src={thumbnail} alt="Thumbnail" className='w-[60px] h-[60px] object-cover rounded-md' />
+        <div className='flex flex-col'>
+          <span className='font-semibold text-white text-sm'>{title?.length > 20 ? `${title.slice(0,20)}...` : title}</span>
+          {artists?.map(item=>{
+              return (
+                  <>
+                      <span className='text-xs text-gray-300 cursor-pointer'>{item?.name}</span>
+                  </>
+              )
+          })}
+        </div>
       </div>
+      <span className="text-bold text-white text-[16px]">{percent}%</span>
     </div>
   )
 }
